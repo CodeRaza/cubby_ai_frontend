ea6395d9d70c37027864b637a239dd38d1fdab81
Ali Raza <coder.raza@gmail.com>
2025-11-24 16:37:15 +0500

    Pixel Installed & Resend Added.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with custom config
 const api = axios.create({
-  baseURL: 'http://localhost:8000',  // Django backend URL
+  baseURL: 'https://backend.getcubby.ai',  // Django backend URL
   headers: {
     'Content-Type': 'application/json',
   },
